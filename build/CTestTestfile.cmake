a6b13af8dbb90d53e9c3893a46247a4a32441945
Charles Lesser <clesser@ucsc.edu>
2025-11-14 11:48:04 -0800

    build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/hydri/OneDrive/Documents/GitHub/robots
+# Build directory: C:/Users/hydri/OneDrive/Documents/GitHub/robots/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
